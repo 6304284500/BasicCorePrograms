72111fe3d771f497de209f2358c2b3d5d00fd828
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-19 20:29:23 +0530

    Add files via upload

---
+++ swap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePrograms
+{
+    public class Swap
+    {
+        public void c()
+        {
+            int number1 = 10, number2 = 20, temp = 0;
+            Console.WriteLine($"Before Swapping number1= {number1}, number2 = {number2}");
+            temp = number1; //temp=10
+            number1 = number2; //number1=20      
+            number2 = temp; //number2=10    
+            Console.WriteLine($"After swapping number1= {number1}, number2 = {number2}");
+            Console.ReadLine();
+        }
+    }
+}
