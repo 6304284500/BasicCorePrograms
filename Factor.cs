f187af113777b154269212d8976e909278b9f163
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-19 20:35:41 +0530

    Add files via upload

---
+++ Factor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePrograms
+{
+    public class Factor
+    {
+        public void e()
+        {
+            int num, x;
+            Console.WriteLine("Enter the Number ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (x = 1; x <= num; x++)
+            {
+                if (num % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
