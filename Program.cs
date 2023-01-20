ed07129c0a4577453b2bb435cd5115c976db3248
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-20 08:44:03 +0530

    Main Program where all functions are called

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using CorePrograms;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //LeapYear year = new LeapYear(); //Calling Leap Year Program
+        //Flipcoin flip = new Flipcoin(); //Calling FlipCoin Program
+        //Swap swap = new Swap();         //Calling Swap the two Program
+        //LargestAmong3 largestAmong3 = new LargestAmong3(); //Calling Largest among 3 Program
+        //Factor factor = new Factor(); //Calling Fator of a Number Program
+        //EvenOdd evenOdd = new EvenOdd();//Calling Even or Odd Program
+        //VowelOrConst vowelOrConst = new VowelOrConst();//Calling Vowel or constant Program
+        Harmonic harmonic = new Harmonic();//Calling Harmonic Number Program
+        QuRe quRe = new QuRe();//Calling COmpute Quiotent Remainder Program
+        //year.a();      // calling 'a' method from Leap Year Program
+        //flip.b();      // calling 'b' method from Flipcoin Program
+        //swap.c();      // calling 'c' method from Swap Program
+        //largestAmong3.d();   // calling 'd' method from Largest Among 3 Program
+        //factor.e();  // calling 'e' method from the Factor of a Number Program
+        //evenOdd.f(); // calling 'f' method from the Even or Odd Program
+       // vowelOrConst.g();// calling 'g' method from the Vowel or constant Program
+        harmonic.h ();// calling 'h' method from the Harmonic Program
+        quRe.i ();// calling 'i' method from the Quiotent Remainder Program
+    }
+}
