77b7d52751b1ce152f2867e8c53477c18f9fb7ae
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-20 08:39:35 +0530

    program to find the Harmonic number

---
+++ Harmonic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePrograms
+{
+    public class Harmonic
+    {
+        public void h()
+        {
+            int i, n;
+            double s = 0.0;
+
+         
+            Console.Write("Calculate the harmonic series and their sum:\n");
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+}
