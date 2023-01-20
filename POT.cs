715e97aa411de61ba5adbff4f40b02d29c1f64a9
Kolavennu Sai Sreeram Kumar <79280855+6304284500@users.noreply.github.com>
2023-01-20 08:42:45 +0530

    Program to find the power of two

---
+++ POT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePrograms
+{
+    internal class POT
+    {
+        public POT()
+        {
+            Console.WriteLine(IsPowerOfTwo(9223372036854775809));
+            Console.WriteLine(IsPowerOfTwo(4));
+            Console.ReadLine();
+        }
+        static bool IsPowerOfTwo(ulong x)
+        {
+            return x > 0 && (x & (x - 1)) == 0;
+        }
+    }
+}
